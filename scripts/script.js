33a27209eef4d1d9e493e9bf6eab5c76899cda02
Willem Sleegers <w.sleegers@me.com>
2021-01-02 17:37:55 +0000

    Updated the website with separate pages for each section and much more information.

--- scripts/script.js
+++ scripts/script.js
@@ -20,43 +20,112 @@ function copyCitation() {
   //
 
   // Place in top-left corner of screen regardless of scroll position.
-  textArea.style.position = 'fixed';
+  textArea.style.position = "fixed";
   textArea.style.top = 0;
   textArea.style.left = 0;
 
   // Ensure it has a small width and height. Setting to 1px / 1em
   // doesn't work as this gives a negative w/h on some browsers.
-  textArea.style.width = '2em';
-  textArea.style.height = '2em';
+  textArea.style.width = "2em";
+  textArea.style.height = "2em";
 
   // We don't need padding, reducing the size if it does flash render.
   textArea.style.padding = 0;
 
   // Clean up any borders.
-  textArea.style.border = 'none';
-  textArea.style.outline = 'none';
-  textArea.style.boxShadow = 'none';
+  textArea.style.border = "none";
+  textArea.style.outline = "none";
+  textArea.style.boxShadow = "none";
 
   // Avoid flash of white box if rendered for any reason.
-  textArea.style.background = 'transparent';
-  
+  textArea.style.background = "transparent";
+
   // Set text
-  textArea.value = "Sleegers, W.W.A. (2020) tidystats: Save output of statistical tests (Version 0.5) [R package]. https://doi.org/10.5281/zenodo.4041859;"
+  textArea.value =
+    "Sleegers, W.W.A. (2020) tidystats: Save output of statistical tests (Version 0.5) [Computer software]. https://doi.org/10.5281/zenodo.4041859;";
 
   document.body.appendChild(textArea);
   textArea.focus();
   textArea.select();
 
   try {
-	var successful = document.execCommand('copy');
-	var msg = successful ? 'successful' : 'unsuccessful';
-	console.log('Copying text command was ' + msg);
-	document.getElementById("citationButton").innerHTML = "Copied!";
-	setTimeout(function() {
-	  document.getElementById("citationButton").innerHTML = "Copy citation";
-	}, 3000);
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Copying text command was " + msg);
+    document.getElementById("citationButton").innerHTML = "Copied!";
+    setTimeout(function () {
+      document.getElementById("citationButton").innerHTML = "Copy citation";
+    }, 3000);
   } catch (err) {
-	console.log('Oops, unable to copy');
+    console.log("Oops, unable to copy");
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyBibTex() {
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakiness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a
+  // flash, so some of these are just precautions. However in
+  // Internet Explorer the element is visible whilst the popup
+  // box asking the user for permission for the web page to
+  // copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = "fixed";
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = "2em";
+  textArea.style.height = "2em";
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = "none";
+  textArea.style.outline = "none";
+  textArea.style.boxShadow = "none";
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = "transparent";
+
+  // Set text
+  textArea.value = `@software{sleegers2020,
+    title = {tidystats: Save output of statistical tests},
+    author = {Sleegers, Willem W. A.},
+    date = {2020},
+    url = {https://doi.org/10.5281/zenodo.4041859},
+    version = {0.5}
+  }`;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Copying text command was " + msg);
+    document.getElementById("bibTexButton").innerHTML = "Copied!";
+    setTimeout(function () {
+      document.getElementById("bibTexButton").innerHTML = "Copy citation";
+    }, 3000);
+  } catch (err) {
+    console.log("Oops, unable to copy");
   }
 
   document.body.removeChild(textArea);
